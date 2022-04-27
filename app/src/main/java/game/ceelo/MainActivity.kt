ac3023c273138e0e65cfec622412826aaaa79091
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-27 20:10:32 +0200

    resize

--- app/src/main/java/game/ceelo/MainActivity.kt
+++ app/src/main/java/game/ceelo/MainActivity.kt
@@ -24,8 +24,6 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
-
 fun loadLocalGame(binding: ActivityMainBinding) = binding.apply {
     playLocalButton.setOnClickListener {
         dicesThrow.apply player@{
