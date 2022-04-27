87986fb2ab2e5129f7d14da724a899dc0d2567d1
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-27 19:52:33 +0200

    refactor some just for fun

--- app/src/main/java/game/ceelo/MainActivity.kt
+++ app/src/main/java/game/ceelo/MainActivity.kt
@@ -13,84 +13,96 @@ import game.ceelo.databinding.ActivityMainBinding.inflate
 import game.ceelo.domain.*
 import game.ceelo.domain.DiceThrowResult.*
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inflate(layoutInflater).apply {
             setContentView(root)
-            laodLocalGame(binding = this)
+            loadLocalGame(binding = this)
         }
     }
 }
 
-private fun throwDiceAnimation(diceImage: ImageView, diceValue: Int) {
-    diceImage.apply {
-        setImageResource(getDiceImageResourcefromDiceValue(diceValue))
-        startAnimation(RotateAnimation(
-            0f,
-            360f,
-            RELATIVE_TO_SELF,
-            0.5f,
-            RELATIVE_TO_SELF,
-            0.5f
-        ).apply {
-            duration = 500
-        })
-    }
-
-}
-
-private fun getDiceImageResourcefromDiceValue(diceValue: Int): Int {
-    return when (diceValue) {
-        ONE -> dice_face_one
-        TWO -> dice_face_two
-        THREE -> dice_face_three
-        FOUR -> dice_face_four
-        FIVE -> dice_face_five
-        SIX -> dice_face_six
-        else -> throw Exception("Only six faces is possible!")
-    }
-}
-//TODO:
-fun setTextViewResult(textViewResult: TextView, diceResult:DiceThrowResult){
-
-}
-private fun laodLocalGame(binding: ActivityMainBinding) {
-    binding.apply {
-        playLocalButton.setOnClickListener {
-            dicesThrow.apply player@{
-                dicesThrow.apply computer@{
-                    throwDiceAnimation(playerOneFirstDiceImageId, this@player.first())
-                    throwDiceAnimation(playerOneMiddleDiceImageId, this@player.middle())
-                    throwDiceAnimation(playerOneLastDiceImageId, this@player.last())
-                    throwDiceAnimation(playerTwoFirstDiceImageId, this@computer.first())
-                    throwDiceAnimation(playerTwoMiddleDiceImageId, this@computer.middle())
-                    throwDiceAnimation(playerTwoLastDiceImageId, this@computer.last())
-                    //TODO: fun setTextViewResult(textViewResult: TextView, diceResult:DiceThrowResult){
-                    localPlayerResultText.apply {
-                        text =
-                            when (this@player.compareThrows(secondPlayerThrow = this@computer)) {
-                                WIN -> WIN.toString()
-                                LOOSE -> LOOSE.toString()
-                                else -> RETHROW.toString()
-                            }
-                        visibility = VISIBLE
-                    }
-
-                    computerResultText.apply {
-                        text =
-                            when (this@computer.compareThrows(secondPlayerThrow = this@player)) {
-                                WIN -> WIN.toString()
-                                LOOSE -> LOOSE.toString()
-                                else -> RETHROW.toString()
-                            }
-                        visibility = VISIBLE
-                    }
-                }
+fun loadLocalGame(binding: ActivityMainBinding) = binding.apply {
+    playLocalButton.setOnClickListener {
+        dicesThrow.apply player@{
+            dicesThrow.apply computer@{
+                throwDiceAnimation(playerOneFirstDiceImageId, this@player.first())
+                throwDiceAnimation(playerOneMiddleDiceImageId, this@player.middle())
+                throwDiceAnimation(playerOneLastDiceImageId, this@player.last())
+                throwDiceAnimation(playerTwoFirstDiceImageId, this@computer.first())
+                throwDiceAnimation(playerTwoMiddleDiceImageId, this@computer.middle())
+                throwDiceAnimation(playerTwoLastDiceImageId, this@computer.last())
+                applyTextViewResult(
+                    localPlayerResultText,
+                    this@player.compareThrows(secondPlayerThrow = this@computer)
+                )
+                applyTextViewResult(
+                    computerResultText,
+                    this@computer.compareThrows(secondPlayerThrow = this@player)
+                )
             }
-
         }
     }
 }
+
+val diceImages: List<Int> by lazy {
+    listOf(
+        dice_face_one,
+        dice_face_two,
+        dice_face_three,
+        dice_face_four,
+        dice_face_five,
+        dice_face_six,
+    )
+}
+
+fun throwDiceAnimation(
+    diceImage: ImageView,
+    diceValue: Int
+) = diceImage.apply {
+    setImageResource(
+        getDiceImageFromDiceValue(
+            diceValue,
+            diceImages
+        )
+    )
+}.run {
+    startAnimation(RotateAnimation(
+        0f,
+        360f,
+        RELATIVE_TO_SELF,
+        0.5f,
+        RELATIVE_TO_SELF,
+        0.5f
+    ).apply { duration = 500 })
+}
+
+fun getDiceImageFromDiceValue(
+    diceValue: Int,
+    diceImages: List<Int>
+): Int = when (diceValue) {
+    ONE -> diceImages[ONE - 1]
+    TWO -> diceImages[TWO - 1]
+    THREE -> diceImages[THREE - 1]
+    FOUR -> diceImages[FOUR - 1]
+    FIVE -> diceImages[FIVE - 1]
+    SIX -> diceImages[SIX - 1]
+    else -> throw Exception("Only six faces is possible!")
+}
+
+fun applyTextViewResult(
+    textViewResult: TextView,
+    diceResult: DiceThrowResult
+): TextView = textViewResult.apply {
+    text = when (diceResult) {
+        WIN -> WIN.toString()
+        LOOSE -> LOOSE.toString()
+        else -> RETHROW.toString()
+    }
+    visibility = VISIBLE
+}
+
+
+
