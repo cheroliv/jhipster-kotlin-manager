c77352b38157befb694790759784457ee8caccba
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-05 15:38:46 +0200

    réapparition du bouton

--- app/src/main/java/game/ceelo/StatsActivity.kt
+++ app/src/main/java/game/ceelo/StatsActivity.kt
@@ -8,6 +8,7 @@ class StatsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         inflate(layoutInflater).apply {
+            setContentView(root)
             backButton.setOnClickListener { finish() }
         }
     }
