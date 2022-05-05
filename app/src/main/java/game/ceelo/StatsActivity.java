1a90f55975bc80314e08af4e0d4b10cfbf9ab43f
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-05-05 11:08:23 +0200

    on a fait un view adapter like a boss

---
+++ app/src/main/java/game/ceelo/StatsActivity.java
@@ -0,0 +1,9 @@
+package game.ceelo;
+
+import android.app.Activity;
+import android.view.View;
+
+public class StatsActivity extends Activity {
+    public void onClickBackButtonEvent(View view) {
+    }
+}
