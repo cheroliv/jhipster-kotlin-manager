b207a961b3c2baefec5d540875924039e5291ca3
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-07 15:23:14 +0200

    add .gitignore to root folder

--- server/gateway/src/main/kotlin/game/ceelo/server/gateway/service/UsernameAlreadyUsedException.kt
+++
@@ -1,7 +0,0 @@
-package game.ceelo.server.gateway.service
-
-class UsernameAlreadyUsedException : RuntimeException("Login name already used!") {
-    companion object {
-        private const val serialVersionUID = 1L
-    }
-}
