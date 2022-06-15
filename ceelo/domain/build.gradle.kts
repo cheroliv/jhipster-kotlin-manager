b02577f04d3a08c5d487933eb9d5c1a6dd85651a
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-06-15 03:05:10 +0200

    refactor rename and clean code

--- ceelo/domain/build.gradle.kts
+++ ceelo/domain/build.gradle.kts
@@ -10,13 +10,10 @@ plugins {
 dependencies {
     // coroutines
 //    implementation(dependencyNotation = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${properties["kotlinx_coroutines_version"]}")
-    // Koin for Kotlin apps
     implementation(dependencyNotation = "io.insert-koin:koin-core:${properties["koin_version"]}")
     testImplementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test")
     testImplementation(dependencyNotation = "org.jetbrains.kotlin:kotlin-test-junit")
-    // Testing koin for Kotlin
     testImplementation (dependencyNotation = "io.insert-koin:koin-test:${properties["koin_version"]}")
-    // Koin for JUnit 4
     testImplementation(dependencyNotation = "io.insert-koin:koin-test-junit4:${properties["koin_version"]}")
 }
 
