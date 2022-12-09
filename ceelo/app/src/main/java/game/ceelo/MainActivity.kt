bb9107a03c49f40546e583b80009206f9a3e5b6c
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-12-09 23:53:07 +0100

    refactor and update dependencies and android version

--- ceelo/app/src/main/java/game/ceelo/MainActivity.kt
+++ ceelo/app/src/main/java/game/ceelo/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import game.ceelo.databinding.ActivityMainBinding.inflate
-import org.koin.android.ext.android.get
-import org.koin.core.context.KoinContext
 
 
 class MainActivity : AppCompatActivity() {
