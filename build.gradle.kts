e66e30c807228eb67bf382a4cc9e55fcd28066f6
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-01-02 01:14:52 +0100

    refactor rename and clean code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+
+
+import java.lang.System.getProperty
+import java.io.ByteArrayOutputStream
+
 buildscript {
     repositories {
         google()
@@ -21,4 +26,29 @@ tasks.register<Delete>("clean") {
     description = "Delete directory build"
     group = "build"
     delete(rootProject.buildDir)
+}
+
+tasks.register<GradleBuild>("serve") {
+    description = "lance le server backend de l'application"
+    val pathSeparator: String = getProperty("file.separator")
+    buildFile = File("${rootDir.path}${pathSeparator}webapp${pathSeparator}build.gradle")
+    tasks = listOf("bootRun")
+}
+
+
+open class GradleStop : Exec() {
+    @Suppress("PropertyName")
+    private val GRADLE_PATH="/.sdkman/candidates/gradle/current/bin/gradle"
+    init {
+        workingDir = project.rootDir
+        @Suppress("LeakingThis")
+        commandLine("${getProperty("user.home")}${GRADLE_PATH}", "--stop")
+        standardOutput = ByteArrayOutputStream()
+    }
+}
+
+project.tasks.register<GradleStop>("gradleStop")
+
+project.tasks.withType<GradleStop> {
+    doLast { logger.info(standardOutput.toString()) }
 }
