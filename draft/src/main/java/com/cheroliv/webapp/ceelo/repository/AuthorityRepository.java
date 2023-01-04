0715f7ed74724a66265042cea60ef80003dd5ead
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-01-04 17:05:44 +0100

    Initial version of CeeloApp generated by generator-jhipster@7.9.3

---
+++ draft/src/main/java/com/cheroliv/webapp/ceelo/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cheroliv.webapp.ceelo.repository;
+
+import com.cheroliv.webapp.ceelo.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
