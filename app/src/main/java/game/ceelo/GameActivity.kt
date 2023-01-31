aa0e562ae3165c7aaa0d5d0fff35633d122c2dec
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-01-31 21:33:45 +0100

    add login & parameter button visible

--- app/src/main/java/game/ceelo/GameActivity.kt
+++ app/src/main/java/game/ceelo/GameActivity.kt
@@ -27,10 +27,7 @@ class GameActivity : AppCompatActivity() {
                         playerTwoLastDice
                     )
                 ),
-                resultUI = listOf(
-                    localPlayerResult,
-                    computerResult
-                )
+                resultUI = listOf(localPlayerResult, computerResult)
             )
         }
     }
