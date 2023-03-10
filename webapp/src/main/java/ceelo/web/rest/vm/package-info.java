f8800c308f11b05ba58a576d7c6ff940819d4436
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-03-10 02:34:45 +0100

    add webapp to cvs.

---
+++ webapp/src/main/java/ceelo/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package ceelo.web.rest.vm;
---
+++ webapp/src/main/webapp/app/config/dayjs.ts
@@ -0,0 +1,21 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import duration from 'dayjs/plugin/duration';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+// jhipster-needle-i18n-language-dayjs-imports - JHipster will import languages from dayjs here
+import 'dayjs/locale/en';
+import 'dayjs/locale/ar-ly';
+import 'dayjs/locale/zh-cn';
+import 'dayjs/locale/fr';
+import 'dayjs/locale/de';
+import 'dayjs/locale/hi';
+import 'dayjs/locale/id';
+import 'dayjs/locale/ja';
+import 'dayjs/locale/ru';
+import 'dayjs/locale/es';
+
+// DAYJS CONFIGURATION
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
+dayjs.extend(relativeTime);
